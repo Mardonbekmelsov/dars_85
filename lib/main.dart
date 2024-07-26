5747fd3253e9861ee82f3c23b2168c4f7a7961ad
Mardonbekmelsov <153820615+Mardonbekmelsov@users.noreply.github.com>
2024-07-26 23:27:03 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:dars_85/views/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String? user;
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:  SplashScreen(),
+    );
+  }
+}
